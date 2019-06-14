d8743cc156db2a19545eacdc9c756d2bf75120b3
LastMisadventure <alex.crino@live.com>
2019-06-13 23:01:35 -0700

    Added a new cmdlet for archiving projects. Updated the exmaple config file with a new item.

--- Foundry/Foundry.psd1
+++ Foundry/Foundry.psd1
@@ -72,7 +72,7 @@
     FunctionsToExport = 'Get-Project', 'Get-RandomReleaseName',
     'Invoke-CodeAnalyzer', 'Invoke-TestSuite',
     'New-MagicQuadrant', 'New-Project', 'Open-Project',
-    'Update-ProjectModuleManifest', 'Update-PsModulePath', 'Remove-Project', 'Test-ProjectReadyForRelease', 'Update-ProjectVsCodeFileSet'
+    'Update-ProjectModuleManifest', 'Update-PsModulePath', 'Remove-Project', 'Test-ProjectReadyForRelease', 'Update-ProjectVsCodeFileSet', 'Move-ProjectToArchive'
 
     # Cmdlets to export from this module, for best performance, do not use wildcards and do not delete the entry, use an empty array if there are no cmdlets to export.
     CmdletsToExport   = @()
---
+++ Foundry/public/Move-ProjectToArchive.ps1
@@ -0,0 +1,59 @@
+<#
+.SYNOPSIS
+Zips a project to the configured archive directory. If the zip is successful, the project is removed.
+
+.DESCRIPTION
+Zips a project to the configured archive directory. If the zip is successful, the project is removed.
+
+.EXAMPLE
+Move-ProjectToArchive -Name Core
+
+.NOTES
+General notes
+#>
+
+function Move-ProjectToArchive {
+
+    [CmdletBinding(PositionalBinding, ConfirmImpact = 'medium')]
+
+    param (
+
+    )
+
+    dynamicParam {
+
+        NewDynamicParameterDictionary -DynamicParameter $script:Scoped_DynamicParams_ProjectNames
+
+    }
+
+    begin {
+
+    }
+
+    process {
+
+        try {
+
+            $project = [Portfolio]::FindOneByNameExact($PsBoundParameters.Name)
+
+            Write-Verbose "[$($MyInvocation.MyCommand.Name)]: $($project.Name) Compressing project directory to $($script:Scoped_ModuleConfig.RepositoryArchive)..."
+
+            Compress-Archive -Path $project.RepositoryPath -DestinationPath (Join-Path -Path $script:Scoped_ModuleConfig.RepositoryArchive -ChildPath $project.Name)
+
+            Write-Verbose "[$($MyInvocation.MyCommand.Name)]: $($project.Name) Compressed project directory. The project will now be removed from the active project directory."
+
+            Foundry\Remove-Project -Name $PsBoundParameters.Name -Confirm:$false
+
+        } catch {
+
+            Write-Error -ErrorAction Stop -Exception $_.Exception
+
+        }
+
+    }
+
+    end {
+
+    }
+
+}
